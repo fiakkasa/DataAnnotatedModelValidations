d1063f2e515cae24fb31522f860a85493aa4cbf8
fiakkasa <fiakkasa@live.com>
2021-03-08 17:54:13 +0200

    Move Validation logic to separate method Add project properties

--- DataAnnotatedModelValidations/ValidatorMiddleware.cs
+++ DataAnnotatedModelValidations/ValidatorMiddleware.cs
@@ -17,53 +17,57 @@ namespace DataAnnotatedModelValidations
             _next = next;
         }
 
-        public async Task InvokeAsync(IMiddlewareContext context)
+        private static void ValidateInputs(IMiddlewareContext context)
         {
             var arguments = context.Field.Arguments;
 
-            if (arguments.Count > 0)
-            {
-                var textInfo = CultureInfo.CurrentCulture.TextInfo;
-                var contextPath = context.Path.ToList().Select(node => new NameString($"{node}")).ToList();
+            if (arguments.Count == 0) return;
 
-                var errors = new List<ValidationResult>();
-                var validationResults =
-                    arguments
-                        .SelectMany(argument =>
-                        {
-                            errors.Clear();
+            var textInfo = CultureInfo.CurrentCulture.TextInfo;
+            var contextPath = context.Path.ToList().Select(node => new NameString($"{node}")).ToList();
 
-                            if (argument is not { } || context.ArgumentValue<object>(argument.Name) is not { } obj)
-                                return Enumerable.Empty<IError>();
+            var errors = new List<ValidationResult>();
+            var validationResults =
+                arguments
+                    .SelectMany(argument =>
+                    {
+                        errors.Clear();
 
-                            Validator.TryValidateObject(obj, new ValidationContext(obj), errors, true);
+                        if (argument is not { } || context.ArgumentValue<object>(argument.Name) is not { } obj)
+                            return Enumerable.Empty<IError>();
 
-                            return errors.Select(validationResult =>
-                                ErrorBuilder.New()
-                                    .SetMessage($"{validationResult.ErrorMessage}")
-                                    .SetPath(
-                                        contextPath
-                                            .Skip(1)
-                                            .Concat(
-                                                validationResult.MemberNames.FirstOrDefault() is string propertyName && propertyName.Length > 0
-                                                    ? new[]
-                                                    {
+                        Validator.TryValidateObject(obj, new ValidationContext(obj), errors, true);
+
+                        return errors.Select(validationResult =>
+                            ErrorBuilder.New()
+                                .SetMessage($"{validationResult.ErrorMessage}")
+                                .SetPath(
+                                    contextPath
+                                        .Skip(1)
+                                        .Concat(
+                                            validationResult.MemberNames.FirstOrDefault() is string propertyName && propertyName.Length > 0
+                                                ? new[]
+                                                {
                                                         new NameString(argument.Name),
                                                         new NameString($"{char.ToLowerInvariant(propertyName[0])}{textInfo.ToTitleCase(propertyName)[1..]}")
-                                                    }
-                                                    : new[] { new NameString(argument.Name) }
-                                            )
-                                            .Aggregate(Path.New(contextPath[0]), (path, segment) => path.Append(segment))
-                                    )
-                                    .SetExtension("field", argument.Coordinate.FieldName)
-                                    .SetExtension("type", argument.Coordinate.TypeName)
-                                    .Build()
-                            );
-                        });
+                                                }
+                                                : new[] { new NameString(argument.Name) }
+                                        )
+                                        .Aggregate(Path.New(contextPath[0]), (path, segment) => path.Append(segment))
+                                )
+                                .SetExtension("field", argument.Coordinate.FieldName)
+                                .SetExtension("type", argument.Coordinate.TypeName)
+                                .Build()
+                        );
+                    });
 
-                foreach (var item in validationResults)
-                    context.ReportError(item);
-            }
+            foreach (var item in validationResults)
+                context.ReportError(item);
+        }
+
+        public async Task InvokeAsync(IMiddlewareContext context)
+        {
+            ValidateInputs(context);
 
             await _next(context).ConfigureAwait(false);
         }
