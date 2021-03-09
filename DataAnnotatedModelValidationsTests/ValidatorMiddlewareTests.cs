792a4e4aceb16a9a89c163311210e6ef11b35b7c
fiakkasa <fiakkasa@live.com>
2021-03-09 22:44:58 +0200

    Add unit tests for inline DataAnnotations, and missing field names

--- DataAnnotatedModelValidationsTests/ValidatorMiddlewareTests.cs
+++ DataAnnotatedModelValidationsTests/ValidatorMiddlewareTests.cs
@@ -1,7 +1,9 @@
 ﻿using HotChocolate;
 using HotChocolate.Execution;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -10,13 +12,25 @@ namespace DataAnnotatedModelValidations.Tests
 {
     public class ValidatorMiddlewareTests
     {
-        public class Sample
+        public class Sample : IValidatableObject
         {
             [Required]
             [MinLength(3)]
-            [MaxLength(8)]
             [EmailAddress]
             public string? Email { get; set; }
+
+            [GraphQLIgnore]
+            public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) =>
+                (
+                    Email?.StartsWith("no-property-name") == true
+                        ? new[] { new ValidationResult("no-property-name") }
+                        : Enumerable.Empty<ValidationResult>()
+                )
+                .Concat(
+                    Email?.StartsWith("empty-property-name") == true
+                        ? new[] { new ValidationResult("empty-property-name", new[] { "" }) }
+                        : Enumerable.Empty<ValidationResult>()
+                );
         }
 
         public class MockService
@@ -26,6 +40,10 @@ namespace DataAnnotatedModelValidations.Tests
 
         public class Query
         {
+            public string? GetText([Required] string? text) => text;
+
+            public string? GetTextIgnoreValidation([IgnoreModelValidation][Required] string? text) => text;
+
             public Sample? GetSample(Sample? sample) => sample;
 
             public Sample GetSampleNonNull(Sample sample) => sample;
@@ -37,11 +55,15 @@ namespace DataAnnotatedModelValidations.Tests
         }
 
         [Theory]
+        [InlineData("{ text(text: null) }", 1)]
+        [InlineData("{ text(text: \"\") }", null)]
+        [InlineData("{ textIgnoreValidation(text: null) }", 1)]
         [InlineData("{ sample(sample: null) { email } }", null)]
         [InlineData("{ sample(sample: { email: null }) { email } }", 1)]
         [InlineData("{ sample(sample: { email: \"\" }) { email } }", 1)]
         [InlineData("{ sample(sample: { email: \"ab\" }) { email } }", 2)]
-        [InlineData("{ sample(sample: { email: \"ab@cd.com\" }) { email } }", 1)]
+        [InlineData("{ sample(sample: { email: \"no-property-name@b.com\" }) { email } }", 1)]
+        [InlineData("{ sample(sample: { email: \"empty-property-name@b.com\" }) { email } }", 1)]
         [InlineData("{ sample(sample: { email: \"a@b.com\" }) { email } }", null)]
         [InlineData("{ sampleNonNull(sample: null) { email } }", 1)]
         [InlineData("{ sampleNonNull(sample: { email: \"ab\" }) { email } }", 2)]
